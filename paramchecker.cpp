9ab0b61d9cd6247dfbebe2b7986980d59d04a97b
Vinay Kumar Shettar <vinaykumar.shettar@in.bosch.com>
2020-07-27 11:56:44 +0000

    Reduce cyclomatic complexity
    Signed-off-by: Vinay Kumar Shettar <vinaykumar.shettar@in.bosch.com>

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,9 +1,34 @@
+bool bmpAreOk(float bpm)
+{
+	if (bpm < 70 || bpm > 150)
+		return false;
+	else 
+		return true;
+}
+
+bool spo2AreOk(float spo2)
+{
+	if (spo2 < 80)
+		return false;
+	else 
+		return true;
+}
+	
+bool respRateAreOk(float respRate)
+{
+	if(respRate < 30 || respRate > 60)
+		return false;
+	else 
+		return true;
+}
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150 || spo2 < 80) {
+  if(!bmpAreOk(bpm))
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  else if(!spo2AreOk(spo2))
     return false;
-  }
+  else if(!respRateAreOk(respRate))
+    return false;
+  
   return true;
 }
