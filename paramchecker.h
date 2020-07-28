4cd9dbdf339027345e0388465984c468103b39a0
Vinay Kumar Shettar <vinaykumar.shettar@in.bosch.com>
2020-07-28 10:50:45 +0000

    Added Range tests
    Signed-off-by: Vinay Kumar Shettar <vinaykumar.shettar@in.bosch.com>

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,2 @@
-
+bool IsRangeOk(float val, float min, float max);
 bool vitalsAreOk(float bpm, float spo2, float respRate);
--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,10 +1,29 @@
 #include "paramchecker.h"
 #include <gtest/gtest.h>
- 
-TEST(VitalsTest, BPM) { 
+#if 0 
+TEST(VitalsTest1, Range) {
+    ASSERT_EQ(true, IsRangeOk(100, 50, 100));
+}
+TEST(VitalsTest2, Range) {
+    ASSERT_EQ(true, IsRangeOk(50, 50, 100));
+}
+TEST(VitalsTest3, Range) {
+    ASSERT_EQ(true, IsRangeOk(60, 50, 100));
+}
+TEST(VitalsTest4, Range) {
+    ASSERT_EQ(false, IsRangeOk(10, 50, 100));
+}
+#endif
+TEST(VitalsTest, Range) {
+    ASSERT_EQ(false, IsRangeOk(120, 50, 100));
+    ASSERT_EQ(true, IsRangeOk(100, 50, 100));
+    ASSERT_EQ(true, IsRangeOk(50, 50, 100));
+    ASSERT_EQ(true, IsRangeOk(60, 50, 100));
+    ASSERT_EQ(false, IsRangeOk(10, 50, 100));
+}
+TEST(VitalsTest, BPM) {
     ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
 }
- 
 TEST(VitalsTest, SPO2) {
     ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
 }
